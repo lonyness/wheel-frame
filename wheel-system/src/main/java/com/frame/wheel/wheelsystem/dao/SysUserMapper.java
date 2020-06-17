967933b0a1d7260899f1c37bda26880d28f80931
wuyuxiang <wuyuxiang1995@live.com>
2020-06-17 10:22:43 +0800

    修改项目数据库目录

--- wheel-system/src/main/java/com/frame/wheel/wheelsystem/dao/SysUserMapper.java
+++ wheel-system/src/main/java/com/frame/wheel/wheelsystem/dao/SysUserMapper.java
@@ -1,18 +1,12 @@
 package com.frame.wheel.wheelsystem.dao;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.frame.wheel.wheelsystem.entity.SysUser;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Mapper
 @Component(value = "sysUserMapper")
 public interface SysUserMapper extends BaseMapper<SysUser> {
 
-    List<SysUser> select(Page page, @Param("ew") QueryWrapper<SysUser> queryWrapper);
 }

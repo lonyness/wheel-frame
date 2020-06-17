6bb6b1cec45bd3919fb8eb140307782ba37c2c29
wuyuxiang <wuyuxiang1995@live.com>
2020-06-17 20:49:57 +0800

    新增wheel-util工具配置模块

---
+++ wheel-util/src/test/java/com/frame/wheel/wheelutil/WheelUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frame.wheel.wheelutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

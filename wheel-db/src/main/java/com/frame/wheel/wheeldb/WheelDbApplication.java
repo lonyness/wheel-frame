a9f663028c4fb73289543a76bd9f1285a9ae0c4c
wuyuxiang <wuyuxiang1995@live.com>
2020-06-19 15:12:19 +0800

    重新提交wheel-db数据配置模块

---
+++ wheel-db/src/main/java/com/frame/wheel/wheeldb/WheelDbApplication.java
@@ -0,0 +1,13 @@
+package com.frame.wheel.wheeldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelDbApplication.class, args);
+    }
+
+}

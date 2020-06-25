74a4b0333742522e75cc9725a68b177c73a2d828
wuyuxiang <wuyuxiang1995@live.com>
2020-06-25 22:43:25 +0800

    shiro登录，利用shiro-redis工具实现登录并缓存

--- wheel-system/src/main/java/com/frame/wheel/wheelsystem/controller/LoginController.java
+++ wheel-system/src/main/java/com/frame/wheel/wheelsystem/controller/LoginController.java
@@ -3,17 +3,11 @@ package com.frame.wheel.wheelsystem.controller;
 import com.frame.wheel.wheelsystem.entity.SysUser;
 import com.frame.wheel.wheelutil.base.controller.BaseController;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
